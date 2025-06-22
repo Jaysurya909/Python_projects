91708b019153e90c29e0055c3b6f749db378a092
atharva <baodhankaratharva@gmail.com>
2025-06-22 13:25:50 +0530

    refactor(order_menu): extract order logic into takeOrder function
    Improve code maintainability by encapsulating order-taking logic in a reusable function. This reduces code duplication and makes the main flow more readable.

--- order_menu.py
+++ order_menu.py
@@ -7,25 +7,28 @@ menu={
 
 Total=0
 
+def takeOrder():
+    Order_1=input("Enter the food you want to buy\t").capitalize()
+    if Order_1 in menu:
+        global Total
+        Total += menu[Order_1]
+    else:
+        print(f"The {Order_1} is not available in our menu!!")
+
+
 print("Welcome to our Hotel Chads Kitchen")
 print("The menu of this hotel is:\n 1.Vadapav: $10\n 2.Samosa: $15\n 3.Coffe: $40\n 4.Pizza: $200\n 5.Burger: $150")
 
-Order_1=input("Enter the food you want to buy\t").capitalize()
-if Order_1 in menu:
-    Total += menu[Order_1]
-else:
-    print(f"The {Order_1} is not available in our menu!!")
+takeOrder()
 
 while True:
     order_2=input("Do you want to add more items? (Yes/No)").capitalize()
     if (order_2=='Yes'):
-        Order_1=input("Enter the food you want to buy\t").capitalize()
-        if Order_1 in menu:
-            Total += menu[Order_1]
-        else:
-            print(f"The {Order_1} is not available in our menu!!")
-    elif(Total==0):
-        print("You didnt order anything!!")
+        takeOrder()
+    elif(order_2=='No'):
+        print("Your order is confirmed")
+        print("Your total order cost is $",Total)
+
         break
     else:
         print("Your total order cost is $",Total)
