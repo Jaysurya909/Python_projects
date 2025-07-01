9f15f008851a45ef557c1de3d4e151471429b9c1
JaySurya <jaysurya6969@gmail.com>
2025-07-01 14:29:19 +0530

    Minor changes to qrcode_generator

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -2,8 +2,8 @@ import qrcode
 import PIL
 
 # Taking the users upi id
-upi_ID=input("Enter your upi id")
-qr_url=int(input("Do you want to generate qr code or a url for payment ?\nEnter 1 for QRcode\nEnter 2 for url genration "))
+upi_ID=input("---- Enter your UPI ID ----\n")
+qr_url=int(input("Do you want to generate QR-code or a URL for payment ?\nEnter 1 for QRcode\nEnter 2 for URL genration\nEnter 3 for Both QR code and URL\n"))
 url_app=None
 
 
@@ -21,32 +21,20 @@ paytm_url = f'intent://upi/pay?pa={upi_ID}&pn=Recipient%20Name#Intent;scheme=upi
 
 # Making the QRcode or URL
 if(qr_url==1):
-    qrcode_qr=qrcode.make(qrcode_url)
+    qrcode_qr=qrcode.make(qrcode_url)# This is the only working QR code
     qrcode_qr.show()
 elif(qr_url==2):
     url_app=int(input("For which app you want to generate the url?\n1.Phonepe\n2.Google_pay\n3.Paytem\n4.Universal Url"))
     if(url_app==1):
-        print("This is your url payment link:-",phonepe_url)
+        print("This is your URL for Phonepe Payment :-",phonepe_url)
     elif(url_app==2):
-        print("This is your url payment link:-",google_pay_url)
+        print("This is your URL for Google pay Payment :-",google_pay_url)
     elif(url_app==3):
-        print("This is your url payment link:-",paytm_url)
+        print("This is your URL for Paytm Payment :-",paytm_url)
     elif(url_app==4):
-        print("This is your url payment link:-",qrcode_url)
-
-
+        print("This is your Universal URL for Payments :-",qrcode_url)
+elif(qr_url==3):
+    qrcode_qr=qrcode.make(qrcode_url)
+    qrcode_qr.show()
+    print("This is your Universal URL for Payments :-",qrcode_url)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
