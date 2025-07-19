c370a89e6e0984599c6346a95d6f7f99b75917ba
JaySurya <jaysurya6969@gmail.com>
2025-07-19 12:56:36 +0530

    auto whatsapp msg sender

---
+++ auto_whatsappsendmsg.py
@@ -0,0 +1,8 @@
+import pywhatkit as py
+import pyautogui
+import time
+
+py.sendwhatmsg('+919373924727','This is an automated msg from jay  so ignore it',12,56)
+
+time.sleep(2)
+pyautogui.press("enter")
