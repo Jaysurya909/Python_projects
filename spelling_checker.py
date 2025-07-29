4d2c2c2f26290bab90747f2dc2b8c69c1dc54a41
JaySurya <jaysurya6969@gmail.com>
2025-07-29 14:32:56 +0530

    returning the misspelled words in the input

--- spelling_checker.py
+++ spelling_checker.py
@@ -2,7 +2,23 @@ from spellchecker import SpellChecker
 
 spell = SpellChecker()
 
-corrector=spell.unknown(['Dp','not','dp','dhat'])
+class spelling:
+    def input(self):
+        text=input("Enter your sentence")
+        self.list=text.split()
+        
+    def corrector(self):
+        self.mistakes=[]
+        for words in self.list:
+            if spell.correction(words)!=words:
+                self.mistakes.append(spell.correction(words))
+        
+        print(self.mistakes)
 
-for words in corrector:
-    print(spell.correction(words))
+        
+a1=spelling()
+a1.input()
+a1.corrector()
+
+
+
