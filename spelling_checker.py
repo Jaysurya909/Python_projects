9b5a797794e663ded7fb06cf283be60d1bc1faa8
JaySurya <jaysurya6969@gmail.com>
2025-07-29 14:50:49 +0530

    complete spellchecker which returns corrected sentence and list of misspelled words

--- spelling_checker.py
+++ spelling_checker.py
@@ -3,23 +3,30 @@ from spellchecker import SpellChecker
 spell = SpellChecker()
 
 class spelling:
-    def input(self):
+    def input(self):    # Taking the user input sentence
         text=input("Enter your sentence")
         self.list=text.split()
         
-    def corrector(self):
+    def corrector(self):    # Correcting the sentence
         self.mistakes=[]
         for i,words in enumerate(self.list):
             if spell.correction(words)!=words:
                 self.mistakes.append(spell.correction(words))
                 self.list[i]=spell.correction(words)
         
-        print(self.mistakes)
-        print(self.list)
-        
+        #print(self.list)
+    
+    def print_corrected(self):  # Printing the corrected sentence
+        print("The misspelled words are",self.mistakes)
+        for i in self.list:
+            print(f"{i} ",end="")
+            
+            
+
 a1=spelling()
 a1.input()
 a1.corrector()
+a1.print_corrected()
 
 
 
